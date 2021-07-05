b42254e9de486e2223052f979dff8ecb5295fc14
jjanmo <jjanmo07@hanmail.net>
2021-07-05 23:12:32 +0900

    feat: make calculator using typescript

---
+++ 02_types_challenges/calculator.ts
@@ -0,0 +1,31 @@
+/**
+ * Let's make a calculator 🧮
+ */
+
+type Operation = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+
+function calculate(type: Operation, x: number, y: number) {
+  switch (type) {
+    case 'add': {
+      return x + y;
+    }
+    case 'substract': {
+      return x - y;
+    }
+    case 'multiply': {
+      return x * y;
+    }
+    case 'divide': {
+      return x / y;
+    }
+    case 'remainder': {
+      return x % y;
+    }
+  }
+}
+
+console.log(calculate('add', 1, 3)); // 4
+console.log(calculate('substract', 3, 1)); // 2
+console.log(calculate('multiply', 4, 2)); // 8
+console.log(calculate('divide', 4, 2)); // 2
+console.log(calculate('remainder', 5, 2)); // 1
