25b99dd85563d7bc63c9bad75e092562e0ccbfdc
jjanmo <jjanmo07@hanmail.net>
2021-08-12 01:12:39 +0900

    add: add comment of code description about utility types

--- 09_advanced/9_8_omit.ts
+++ 09_advanced/9_8_omit.ts
@@ -17,4 +17,18 @@
     title: 'kingdom',
     url: 'https://....',
   };
+
+  // 실제 Omit type 분석하기
+  type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+  // 어떠한 타입의 키든 그 키가 K에서 확장된 것인데, K를 T에서 뺀 것
+  // -> PicK~~~ 제외하고 남은 것(키)를 고르는 것
+
+  // Exclude<Type, ExcludedUnion> : exclude 배제하다, 채택하지않다
+  // -> Type에서 ExcludedUnion을 배제한 모든 타입
+  type Exclude<T, U> = T extends U ? never : T;
+
+  // K는 타입 T의 키에 속한 것으로서 특정 키를 뽑아서 넣게되면 그것만을 P라고 하면 해당 타입만을 같은 타입으로 지정할 수 있다.
+  type Pick<T, K extends keyof T> = {
+    [P in K]: T[P];
+  };
 }
