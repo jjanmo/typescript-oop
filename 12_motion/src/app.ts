7fdf82764d3ebf5fdb87ffcdf0a55bee9a0010ba
jjanmo <jjanmo07@hanmail.net>
2022-06-18 16:30:33 +0900

    feat: add nav-buttons click event

--- 12_motion/src/app.ts
+++ 12_motion/src/app.ts
@@ -1,1 +1,22 @@
+class App {
+  constructor() {
+    const $buttons = document.querySelectorAll('.upload-button');
 
+    $buttons.forEach(($button) =>
+      $button.addEventListener('click', this.handleClick)
+    );
+  }
+
+  handleClick(e: Event) {
+    const target = e.target as HTMLButtonElement;
+    const type = target.dataset.type as MotionType;
+
+    const $dialogWrapper = document.querySelector(
+      '.dialog-wrapper'
+    ) as HTMLDivElement;
+    $dialogWrapper?.classList.remove('hidden');
+    new Dialog($dialogWrapper, type);
+  }
+}
+
+new App();
